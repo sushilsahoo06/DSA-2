d520e2537b533b57876d320613efa5de28f254c1
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-13 21:26:57 +0530

    new commit

--- 1_parton_pritting/pattern_19.cpp
+++ 1_parton_pritting/pattern_19.cpp
@@ -4,16 +4,19 @@ int main(){
   int n;
   cout<<"enter a number :";
   cin>>n;
-  // for (int i=1;i<=n;i++){
-  //   int j=1;
-  //   while(j<=i){
-
-  //   }
-  // }
-  for (int i=n;i>=1;i--){
-    for(int j=1;j<=i;j++){
+  for (int i=1;i<=n;i++){
+    int star=n-i+1;
+    int j=1;
+    while(j<=star){
       cout<<"*";
+      j++;
     }
     cout<<endl;
   }
+  // for (int i=n;i>=1;i--){
+  //   for(int j=1;j<=i;j++){
+  //     cout<<"*";
+  //   }
+  //   cout<<endl;
+  // }
 }
--- 1_parton_pritting/pattern_20.cpp
+++ 1_parton_pritting/pattern_20.cpp
@@ -3,12 +3,17 @@ using namespace std;
 int main(){
   int n;
   cin>>n;
-  for(int i=n;i>=1;i--){
-    cout<<"*";
-    int space=n-i;
-    while(space){
-      cout<<" ";
+  for(int i=1;i<=n;i++){
+    int star=n-i+1;
+    while(star){
+      cout<<"*";
     }
-    cout<<endl;
+    int j=1;
+    int space=i-1;
+    while(j<=i){
+      cout<<" ";
+      j++;
+    }
   }
+    cout<<endl;
 }
