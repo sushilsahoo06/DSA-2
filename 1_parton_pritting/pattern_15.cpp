05266e10893a5ef41efcfa3a35f60addced6f0ed
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-13 14:03:31 +0530

    new commit

--- 1_parton_pritting/pattern_15.cpp
+++ 1_parton_pritting/pattern_15.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int 
+  int n;
+  cout<<"Enter a number :";
+  cin>>n;
+
+  // for(int i=1;i<=n;i++){
+  //   for(int j=1;j<=i;j++){
+  //     char ch='A'+i+j-2;
+  //     cout<<ch;
+  //   }
+  //   cout<<endl;
+  // }
+  int i=1;
+  while(i<=n){
+    int j=1;
+    while(j<=i){
+      char ch='A'+i+j-2;
+      cout<<ch;
+      j++;
+    }
+    cout<<endl;
+    i++;
+  }
 }
