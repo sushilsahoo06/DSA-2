032c1ec0529ea48c6752e5aff2c7524b3ad5a21e
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-14 19:50:22 +0530

    new commit

--- 2/1.cpp
+++ 2/1.cpp
@@ -7,5 +7,11 @@ int main(){
   cout<<"a|b "<<(a|b)<< endl;
   cout<<"~a "<<(~a)<< endl;
   cout<<"a^b "<<(a^b)<< endl;
+  cout<<"15<<2 "<<(15<<2)<<endl;
+  cout<<"15>>2 "<<(15>>2)<<endl;
+
+  int i=3;
+  cout<<"sum "<<a+(i++)<<endl;
+  cout<<i;
 
 }
---
+++ 2/2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a=4;
+
+  int i=3;
+  // cout<<"sum "<<a+(++i)<<endl;
+  // cout<<i;
+
+  cout<<a+(i--)<<endl;
+  cout<<i;
+
+}
