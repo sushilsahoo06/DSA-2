46f03965824dfc70f0076aca546e1bda5d3bc1cd
sushilsahoo06 <sahoosushil456@gmail.com>
2025-06-16 18:09:13 +0530

    new commit

--- 2/8_loop.cpp
+++ 2/8_loop.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 int main(){
-  int n;
+  long long n;
   cin>>n;
-  while(n>0){
-    int lastdigit=n%10;
-    cout<<lastdigit;
+  int isNegative=(n<0)?-1:1;//store -ve
+  n=abs(n);
+
+  long long rev=0;
+  while(n!=0){
+    int last_digits=n%10;
+    rev=rev*10+last_digits;
     n=n/10;
+    if(rev>INT_MAX || rev<INT_MIN){
+      cout<<"0";
+    }
   }
+  cout<<rev*isNegative;
 }
+///reverse number
