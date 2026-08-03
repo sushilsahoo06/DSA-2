e0734a31f758c48b10c9d6a5d720597e8cf92dde
Sushil Sahoo <160878289+sushilsahoo06@users.noreply.github.com>
2026-08-04 01:25:57 +0530

    Time: 0 ms (100%), Space: 9.6 MB (68.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int t[23][23];
-    int predict(int i,int j,vector<int>& nums){
-        if(i > j) return 0;
-        if(i == j) return nums[i];
-        if(t[i][j] != -1) return t[i][j];
 
-        int take_i=nums[i] + min(predict(i+2,j,nums),predict(i+1,j-1,nums));
-        int take_j=nums[j] + min(predict(i+1,j-1,nums),predict(i,j-2,nums));
-        return t[i][j]=max(take_i,take_j);
+    int predictDiff(int i, int j, vector<int>& nums) {
+        if (i == j) return nums[i];
+        if (t[i][j] != -1) return t[i][j];
+
+        // Score added by picking left/right minus what the opponent gets next
+        int take_i = nums[i] - predictDiff(i + 1, j, nums);
+        int take_j = nums[j] - predictDiff(i, j - 1, nums);
+
+        return t[i][j] = max(take_i, take_j);
     }
-    bool predictTheWinner(vector<int>& nums) {
-        memset(t,-1,sizeof(t));
-        int n=nums.size();
-        int total=accumulate(nums.begin(),nums.end(),0);
-        int player_1=predict(0,n-1,nums);
-        int player_2=total - player_1;
 
-        return player_1 >= player_2;
+    bool predictTheWinner(vector<int>& nums) {
+        memset(t, -1, sizeof(t));
+        return predictDiff(0, nums.size() - 1, nums) >= 0;
     }
 };
